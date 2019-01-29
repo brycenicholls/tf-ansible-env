76878eadd42b1ef1a6af27c68d92025b7d737ee3
Bryce Nicholls <bnicholls@ukcloud.com>
2019-01-29 17:36:19 +0000

    edits

--- security.tf
+++ security.tf
@@ -15,4 +15,11 @@ resource "openstack_compute_secgroup_v2" "ansible_secgroup" {
     ip_protocol = "tcp"
     cidr        = "${module.ci-env.dc-ingress-2}"
   }
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "${module.ci-env.dc-ingress-3}"
+  }
+
 }
